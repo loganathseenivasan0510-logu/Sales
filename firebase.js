4c261bc456b40d2018150d47348e27fbc2ed7995
loganathseenivasan0510-logu <loganathseenivasan0510@gmail.com>
2026-01-28 14:27:36 +0530

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import Firebase App Module
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+// Import Firestore Module
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// 🔥 Paste your Firebase Config here
+const firebaseConfig = {
+  apiKey: "AIzaSyDBIzNoDGZjTpey9IFezHC1fPcbSG28cPk",
+  authDomain: "quotationdashboard1.firebaseapp.com",
+  projectId: "quotationdashboard1",
+  storageBucket: "quotationdashboard1.firebasestorage.app",
+  messagingSenderId: "503074048994",
+  appId: "1:503074048994:web:4e7727745165f6de3547bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore Database
+export const db = getFirestore(app);
