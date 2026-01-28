3cb599bbd88946d5888c4f27e529871c6b23948e
loganathseenivasan0510-logu <loganathseenivasan0510@gmail.com>
2026-01-28 14:42:51 +0530

    Update quotation.js

--- quotation.js
+++ quotation.js
@@ -21,3 +21,30 @@ window.submitQuotation = async function () {
 
   alert("Quotation Saved Successfully!");
 };
+
+import {
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+window.searchQuotation = async function () {
+
+  let searchNo =
+    document.getElementById("searchQuotationNo").value;
+
+  const ref = doc(db, "quotations", searchNo);
+  const snap = await getDoc(ref);
+
+  if (snap.exists()) {
+
+    let data = snap.data();
+
+    document.getElementById("customer").value = data.customer;
+    document.getElementById("status").value = data.status;
+    document.getElementById("value").value = data.value;
+
+    alert("Quotation Found!");
+
+  } else {
+    alert("Quotation Not Found!");
+  }
+};
